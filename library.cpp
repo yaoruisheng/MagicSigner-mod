e5a6fd82bb51cf42c381cb6dafcfd51d72aa7385
yaoruisheng <yaoruisheng@gmail.com>
2025-08-06 13:42:52 +0800

    Update library.cpp

--- library.cpp
+++ library.cpp
@@ -12,7 +12,6 @@
 //  PERFORMANCE OF THIS SOFTWARE.
 
 #include <Windows.h>
-
 #include <cstdint>
 
 HINSTANCE get_original_dll() {
@@ -53,58 +52,117 @@ EXTERN_C __declspec(dllexport) HRESULT STDAPICALLTYPE CreateXmlWriterOutputWithE
   return get_original<fnCreateXmlWriterOutputWithEncodingName>("CreateXmlWriterOutputWithEncodingName")(pOutputStream, pMalloc, pwszEncodingName, ppOutput);
 }
 
-void* follow_jumps(void* p) {
-  auto pb = (uint8_t*)p;
-  while (true) {
-    if (pb[0] == 0xEB) {
-      pb += 2 + (intptr_t) * ((int8_t*)&pb[1]);
-      continue;
+inline void* follow_jumps(void* p) {
+    uint8_t* pb = (uint8_t*)p;
+    while (true) {
+        if (pb[0] == 0xEB) {  // jmp rel8
+            pb += 2 + *(int8_t*)&pb[1];
+            continue;
+        }
+        if (pb[0] == 0xE9) {  // jmp rel32
+            pb += 5 + *(int32_t*)&pb[1];
+            continue;
+        }
+#ifdef _M_X64
+        if (pb[0] == 0xFF && pb[1] == 0x25) { // jmp [rip+imm32]
+            int32_t rel = *(int32_t*)&pb[2];
+            pb = *(uint8_t**)(pb + 6 + rel);
+            continue;
+        }
+        if (pb[0] == 0x48 && pb[1] == 0xFF && pb[2] == 0x25) { // jmp [rip+imm32] with 48 prefix
+            int32_t rel = *(int32_t*)&pb[3];
+            pb = *(uint8_t**)(pb + 7 + rel);
+            continue;
+        }
+#else
+        if (pb[0] == 0xFF && pb[1] == 0x25) { // jmp [imm32]
+            uint32_t addr = *(uint32_t*)&pb[2];
+            pb = *(uint8_t**)(uintptr_t)(addr);
+            continue;
+        }
+#endif
+        break;
     }
-    if (pb[0] == 0xE9) {
-      pb += 5 + (intptr_t) * ((int32_t*)&pb[1]);
-      continue;
+    return pb;
+}
+
+inline BOOL hook(void* fn, void* hook_fn) {
+    uint8_t* patch = (uint8_t*)follow_jumps(fn);
+
+#ifdef _M_X64
+    struct __declspec(align(8)) {
+        uint8_t jmp[8] = { 0xFF, 0x25, 0x02, 0x00, 0x00, 0x00, 0x90, 0x90 }; // jmp [RIP+2] + 2 nop
+        void* addr;
+    } s;
+    s.addr = hook_fn;
+
+    DWORD oldProtect;
+    if (!VirtualProtect(patch, sizeof(s), PAGE_EXECUTE_READWRITE, &oldProtect)) {
+        return FALSE;
     }
-    if (pb[0] == 0xFF && pb[1] == 0x25) {
-      pb = *(uint8_t**)(pb + 6 + (intptr_t) * ((int32_t*)&pb[2]));
-      continue;
+
+    SIZE_T written = 0;
+    BOOL res = WriteProcessMemory(GetCurrentProcess(), patch, &s, sizeof(s), &written);
+    if (!res || written != sizeof(s)) {
+        VirtualProtect(patch, sizeof(s), oldProtect, &oldProtect);
+        return FALSE;
     }
-    if (pb[0] == 0x48 && pb[1] == 0xFF && pb[2] == 0x25) {
-      pb = *(uint8_t**)(pb + 7 + (intptr_t) * ((int32_t*)&pb[3]));
-      continue;
+
+    if (!VirtualProtect(patch, sizeof(s), oldProtect, &oldProtect)) {
+        return FALSE;
     }
-    break;
-  }
-  return pb;
+
+    if (!FlushInstructionCache(GetCurrentProcess(), patch, sizeof(s))) {
+        return FALSE;
+    }
+
+    return TRUE;
+
+#else
+    uint8_t s[5];
+    s[0] = 0xE9; // JMP rel32
+    intptr_t relAddr = (intptr_t)hook_fn - (intptr_t)patch - 5;
+    memcpy(s + 1, &relAddr, 4);
+
+    DWORD oldProtect;
+    if (!VirtualProtect(patch, 5, PAGE_EXECUTE_READWRITE, &oldProtect)) {
+        return FALSE;
+    }
+
+    SIZE_T written = 0;
+    BOOL res = WriteProcessMemory(GetCurrentProcess(), patch, s, 5, &written);
+    if (!res || written != 5) {
+        VirtualProtect(patch, 5, oldProtect, &oldProtect);
+        return FALSE;
+    }
+
+    if (!VirtualProtect(patch, 5, oldProtect, &oldProtect)) {
+        return FALSE;
+    }
+
+    if (!FlushInstructionCache(GetCurrentProcess(), patch, 5)) {
+        return FALSE;
+    }
+
+    return TRUE;
+#endif
 }
 
 LONG WINAPI hooked_CertVerifyTimeValidity(LPFILETIME pTimeToVerify, PCERT_INFO pCertInfo) {
-  return 0;
+    return 0;
 }
 
 VOID WINAPI hooked_GetSystemTimeAsFileTime(LPFILETIME lpSystemTimeAsFileTime) {
-  *lpSystemTimeAsFileTime = {};
-}
-
-static BOOL hook(void* fn, void* hook_fn) {
-  const auto patch = follow_jumps(fn);
-
-  struct hkstruct {
-    uint8_t arr[8] = {0xff, 0x25, 2, 0, 0, 0, 0x90, 0x90};
-    void* p{};
-  } s;
-
-  s.p = hook_fn;
-  SIZE_T w{};
-  return WriteProcessMemory((HANDLE)-1, patch, &s, sizeof(s), &w);
+    *lpSystemTimeAsFileTime = {};
 }
 
 static void initialize() {
-  hook((void*)&CertVerifyTimeValidity, (void*)hooked_CertVerifyTimeValidity);
-  hook((void*)&GetSystemTimeAsFileTime, (void*)hooked_GetSystemTimeAsFileTime);
+    hook((void*)&CertVerifyTimeValidity, (void*)hooked_CertVerifyTimeValidity);
+    hook((void*)&GetSystemTimeAsFileTime, (void*)hooked_GetSystemTimeAsFileTime);
 }
 
 extern "C" BOOL WINAPI DllEntry(HINSTANCE, DWORD reason, LPVOID) {
-  if (reason == DLL_PROCESS_ATTACH)
-    initialize();
-  return TRUE;
+    if (reason == DLL_PROCESS_ATTACH)
+        initialize();
+    return TRUE;
 }
